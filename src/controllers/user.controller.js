82d9ed2e5dec0a3d02441c3ade316847911ab103
1anshu-stack <anshuu424@gmail.com>
2024-11-29 14:47:10 +0530

    resolve the error of coverImage

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -27,7 +27,7 @@ const registerUser = asyncHandler( async (req, res) => {
     }
 
     const existedUser = await User.findOne({
-        $or: [{username}, {fullname}]
+        $or: [{username}, {email}]
     })
 
     if(existedUser){
@@ -35,18 +35,27 @@ const registerUser = asyncHandler( async (req, res) => {
     }
 
     const avatarLocalPath = req.files?.avatar[0]?.path;
-    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+    // const coverImageLocalPath = req.files?.coverImage[0]?.path;
 
+    let coverImageLocalPath
+    if(req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0){
+        coverImageLocalPath = req.files.coverImage[0].path;
+    }
+
+ 
     if(!avatarLocalPath){
         throw new ApiError(400, "Avatar file is required")
     }
 
+
     const avatar = await uploadOnCloudinary(avatarLocalPath)
     const coverImage = await uploadOnCloudinary(coverImageLocalPath)
+
 
     if(!avatar){
         throw new ApiError(400, "Avatar file is required to upload on cloudinary")
     }
+
 
     const user = await User.create({
         fullname,
